06213aebdcfbb0d48acead24d4e0ff46a91436c2
Dezolder <evgeniy.zhukov.2012@gmail.com>
2022-11-29 13:49:01 +0200

    localhost отвечает на GET запрос

--- server/app.js
+++ server/app.js
@@ -3,10 +3,12 @@ const mongoose = require("mongoose");
 const chalk = require("chalk");
 const config = require("config");
 const initDatabase = require("./startup/initDatabase");
+const routes = require('./routes')
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use('/api', routes)
 
 const PORT = config.get("port") ?? 8080;
 
