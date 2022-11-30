5d9d74cfd152405eb12cb5f76cebd9f1cd7c226e
Dezolder <evgeniy.zhukov.2012@gmail.com>
2022-12-01 00:31:42 +0200

    На сервере добавлена логика по работе с токенами, логинами и поролями

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,5 +2,6 @@ const express = require('express')
 const router = express.Router({mergeParams: true})
 
 router.use('/select-master', require('./master.routes'))
+router.use('/auth', require('./auth.routes'))
 
 module.exports = router
