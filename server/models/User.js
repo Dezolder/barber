404b7024512033525c2cba56f832dc0443773010
Dezolder <evgeniy.zhukov.2012@gmail.com>
2022-12-01 03:28:32 +0200

    На сервере добавлен рут user для упраления пользователями

--- server/models/User.js
+++ server/models/User.js
@@ -4,11 +4,11 @@ const schema = new Schema({
   name: {type: String},
   email: {type: String, required: true, unique: true},
   password: {type: String},
-  completedMeetings: Number,
+  // completedMeetings: Number,
   image: String,
-  profession: { type: Schema.Types.ObjectId, ref: 'Profession' },
-  qualities: [{ type: Schema.Types.ObjectId, ref: 'Quality' }],
-  rate: Number,
+  // profession: { type: Schema.Types.ObjectId, ref: 'Profession' },
+  // qualities: [{ type: Schema.Types.ObjectId, ref: 'Quality' }],
+  // rate: Number,
   sex: {type: String, enum: ['male', 'female', 'other']}
 }, {
   timestamps: true
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,5 +3,6 @@ const router = express.Router({mergeParams: true})
 
 router.use('/select-master', require('./master.routes'))
 router.use('/auth', require('./auth.routes'))
+router.use("/user", require("./user.routes"));
 
 module.exports = router
